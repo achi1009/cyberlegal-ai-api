e585c98b2ef1221ce877fa11c0f2bb9e9044aad3
achi1009 <85974066+achi1009@users.noreply.github.com>
2025-04-08 16:33:27 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://your-frontend-domain.com",
+    "https://cyber-legal-ai.onrender.com/",
 ]
 
 app.add_middleware(
